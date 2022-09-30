9e682754528690f59d8823f3bbafdf9622f64f5a
quoctruong1106 <truongb1913349@student.ctu.edu.vn>
2022-09-30 10:05:39 +0700

    Cai dat express chay thong bao xin chao

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const cors = require("cors");
 
-const ApiError = require("./app/api-error");
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
 
 
 app.get("/", (req, res)=> {
